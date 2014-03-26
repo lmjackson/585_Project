6219666af6f5734c2cc881fafa21322d82671a5d
lmjackson <ljack@iastate.edu>
2014-03-26 14:44:05 -0500

    project stuff

---
+++ project ideas.R
@@ -0,0 +1,26 @@
+# Winston Churchill
+# JFK
+# MLK Jr.
+# LBJ
+# Ronald Reagan
+# Nelson Mandela
+# Malala Yousafazi
+# Dick Costolo
+# Pope Francis
+# Debbie Sterling
+# Dr. Brene Brown
+# FDR 1933
+
+
+# Margaret Thatcher
+# James Clapper
+# Paula Deen
+# John Boehner
+# Harry Reid
+# Chip Wilson
+# Richie Incognito
+# Mike Rice
+# Coach Joseph
+# Miley Cyrus
+
+#both: Obama! Good: 2006, 2008. Bad: 2009, 2010, 2011
